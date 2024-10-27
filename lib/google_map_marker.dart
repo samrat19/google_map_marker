7d8ff2b2a6e844d608c585517a9acd62b0cae8fa
= <babinsamrat123@gmail.com>
2024-10-27 09:53:44 +0530

    makes some changes

--- lib/google_map_marker.dart
+++ lib/google_map_marker.dart
@@ -8,12 +8,27 @@ import 'dart:ui' as ui;
 
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+///here is the class
+///
+///
 abstract class GoogleMapMarker {
+
+  ///this function returns a [Future] type of [BitmapDescriptor]
+  ///
+  ///
   static Future<BitmapDescriptor> getMarker(BuildContext context, String imagePath) async {
+
+    ///load the image path from asset
+    ///
+    ///
     ByteData byteData = await DefaultAssetBundle.of(context).load(imagePath);
     ui.Codec codec = await ui
         .instantiateImageCodec(byteData.buffer.asUint8List(), targetWidth: 100);
     ui.FrameInfo fi = await codec.getNextFrame();
+
+    ///converts to [Uint8List]
+    ///
+    ///
     Uint8List imageData =
         (await fi.image.toByteData(format: ui.ImageByteFormat.png))!
             .buffer
